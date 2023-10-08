8999f97cfbb2f70c8e04fc16f3904273177b6a66
torbenconto <torben@2x1.dev>
2023-10-08 01:56:08 -0400

    font, start terminal input, css

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import "./Terminal.css"
+import TerminalInput from "./TerminalInput";
 
 function Terminal() {
     return (
-        <div className="w-full h-full terminal-main-bg">
-
+        <div className="w-full h-full terminal-main-bg p-2 cascadia-code">
+            <TerminalInput />
         </div>
     )
 }
---
+++ src/components/TerminalInput.tsx
@@ -0,0 +1,12 @@
+import "./TerminalInput.css"
+
+function TerminalInput() {
+    return (
+        <p className="font-semibold flex name-color">torbenconto
+            <p className="font-normal at-symbol-color">@</p>
+            <p className="font-normal location-color">nhhs</p>
+        </p>
+    )
+}
+
+export default TerminalInput;
