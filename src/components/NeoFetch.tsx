a98cf319df8292878ef8966ba081fad6dbe53b75
torbenconto <torben@2x1.dev>
2023-10-08 06:20:32 -0400

    finish bad neofetch, icons, layout

--- src/components/NeoFetch.tsx
+++ src/components/NeoFetch.tsx
@@ -1,26 +1,48 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare, faYoutubeSquare } from "@fortawesome/free-brands-svg-icons";
+
 function NeoFetch() {
-    return (
-      <div className="md:flex w-full">
-        <div className="md:w-1/2">
-          <img src="143856272.png" className="rounded-full mx-auto" />
-        </div>
-        <div className="md:w-1/2 text-white md:text-lg text-md">
-          <h1 className="md:text-xl text-lg">Torben Conto @ North Hall High School</h1>
-          <p className="font-semibold">- - - - - - - - - - - - - - - -</p>
-          <strong>Age:</strong> 14 (freshman/9th grade)
-          <h1>
-            <strong>Role:</strong> Intermediate Web/Systems developer studying CS
-          </h1>
-          <h1>
-            <strong>Experience:</strong> ~3 years overall.
-          </h1>
-          <h1>
-            <strong>My Tools:</strong> TypeScript, Golang, C (learning), Various web and server frameworks.
-          </h1>
+  return (
+    <div className="md:flex w-full">
+      <div className="md:w-1/2">
+        <img src="143856272.png" className="rounded-full mx-auto" alt="Profile" />
+      </div>
+      <div className="md:w-1/2 text-white md:text-lg text-md space-y-1">
+        <h1 className="md:text-xl text-lg">Torben Conto @ North Hall High School</h1>
+        <p className="font-semibold">- - - - - - - - - - - - - - - -</p>
+        <strong>Age:</strong> 14 (freshman/9th grade)
+        <h1>
+          <strong>Role:</strong> Intermediate Web/Systems developer studying CS
+        </h1>
+        <h1 className="py-1">
+          <strong>Experience:</strong> ~3 years overall.
+        </h1>
+        <h1 className="py-1">
+          <strong>My Tools:</strong> TypeScript, Golang, C (learning), Various web and server frameworks.
+        </h1>
+        <h1 className="py-1">
+          <strong>Projects:</strong> Enter "projects" in the terminal and press enter(return) to view a full list of my projects.
+        </h1>
+        <h1 className="py-4">
+          <strong>Philosopy:</strong> To write code that prioritizes simplicity, clarity, and maintainability, making it easy for both myself and others to understand and work with. Embrace continuous learning and collaboration to create robust, reliable, and ethical software solutions
+        </h1>
+        <div className="flex h-12 w-1/3 space-x-2">
+          <a href="https://github.com/your-github-profile">
+            <FontAwesomeIcon
+              icon={faGithubSquare}
+              className="w-12 h-full hover:text-gray-400"
+            />
+          </a>
+          <a href="https://www.youtube.com/your-youtube-channel">
+            <FontAwesomeIcon
+              icon={faYoutubeSquare}
+              className="w-12 h-full hover:text-gray-400"
+            />
+          </a>
         </div>
       </div>
-    );
-  }
-  
-  export default NeoFetch;
-  
+    </div>
+  );
+}
+
+export default NeoFetch;
