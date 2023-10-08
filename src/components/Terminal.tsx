3105b23419e4c8c56085c356e9b99c9a8e059723
torbenconto <torben@2x1.dev>
2023-10-08 04:09:28 -0400

    refactor, rework

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -4,6 +4,10 @@ import "./Terminal.css"
 import TerminalInput from "./TerminalInput";
 
 function Terminal() {
+
+    let terminal: any = [
+    ];
+
     return (
         <div className="w-full h-full terminal-main-bg p-2 cascadia-code">
             <TerminalInput />
--- src/components/TerminalInput.tsx
+++ src/components/TerminalInput.tsx
@@ -1,18 +1,78 @@
-import "./TerminalInput.css"
+import React, { useState, useRef, useEffect, FC } from "react";
+import "./TerminalInput.css";
 
-function TerminalInput() {
-    return (
-        <>
-        <div className="flex space-x-1">
-            <p className="font-semibold flex name-color">torbenconto
-                <p className="font-normal at-symbol-color">@</p>
-                <p className="font-normal location-color">nhhs</p>
-                <p className="font-semibold arrow-color ml-1">&gt;</p>
-            </p>
-            <input className="input-box px-1 text-white" />
-        </div>
-        </>
-    )
-}
+type InputProps = {
+  setDone: (done: boolean) => void;
+  autoFocus: boolean;
+  initialCommand?: string;
+};
+
+const Input: FC<InputProps> = ({ setDone, autoFocus, initialCommand }) => {
+  const [output, setOutput] = useState<string>("");
+  const [hasRunInitialCommand, setHasRunInitialCommand] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (autoFocus && inputRef.current) {
+      inputRef.current.focus();
+    }
+
+    if (!hasRunInitialCommand && initialCommand) {
+      setOutput("Running initial command...");
+      setDone(true);
+      setHasRunInitialCommand(true);
+    }
+  }, [autoFocus, hasRunInitialCommand, initialCommand, setDone]);
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setOutput(e.currentTarget.value);
+      setDone(true);
+      e.currentTarget.disabled = true;
+    }
+  };
+
+  return (
+    <>
+      <div className="flex space-x-1">
+        <p className="font-semibold flex name-color">
+          torbenconto
+          <p className="font-normal at-symbol-color">@</p>
+          <p className="font-normal location-color">nhhs</p>
+          <p className="font-semibold arrow-color ml-1">&gt;</p>
+        </p>
+        <input
+          className="input-box px-1 text-white"
+          onKeyDown={handleKeyPress}
+          ref={inputRef}
+          disabled={!autoFocus}
+        />
+      </div>
+      {output && <p>{output}</p>}
+    </>
+  );
+};
+
+const TerminalInput: FC = () => {
+  const [done, setDone] = useState(false);
+  const [inputs, setInputs] = useState<JSX.Element[]>([
+    <Input key={0} setDone={setDone} autoFocus={true} initialCommand="neofetch" />,
+  ]);
+
+  const addNewInput = () => {
+    if (done) {
+      const newInputs = [...inputs];
+      newInputs.push(<Input key={newInputs.length} setDone={setDone} autoFocus={true} />);
+      setInputs(newInputs);
+      setDone(false);
+    }
+  };
+
+  useEffect(() => {
+    addNewInput();
+  }, [done]);
+
+  return <>{inputs}</>;
+};
 
 export default TerminalInput;
