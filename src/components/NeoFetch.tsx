18114f228cefa11220056dea442eb7e024fb4685
torbenconto <torben@2x1.dev>
2023-10-08 08:31:26 -0400

    links

--- src/components/NeoFetch.tsx
+++ src/components/NeoFetch.tsx
@@ -27,13 +27,13 @@ function NeoFetch() {
           <strong>Philosopy:</strong> To write code that prioritizes simplicity, clarity, and maintainability, making it easy for both myself and others to understand and work with. Embrace continuous learning and collaboration to create robust, reliable, and ethical software solutions
         </h1>
         <div className="flex h-12 w-1/3 space-x-2">
-          <a href="https://github.com/your-github-profile">
+          <a href="https://github.com/torbenconto">
             <FontAwesomeIcon
               icon={faGithubSquare}
               className="w-12 h-full hover:text-gray-400"
             />
           </a>
-          <a href="https://www.youtube.com/your-youtube-channel">
+          <a href="https://www.youtube.com/channel/UCFvkacmg5QC_NgYYhfRVkxA">
             <FontAwesomeIcon
               icon={faYoutubeSquare}
               className="w-12 h-full hover:text-gray-400"
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-function ProjectCard({ title, description, image }: any) {
+function ProjectCard({ title, description, image, link }: any) {
   return (
-    <div className="bg-gray-900 text-white p-4 rounded-lg shadow-md hover:scale-105 transform transition-transform duration-300">
-      <img
-        src={image}
-        alt={title}
-        className="w-full h-32 object-cover mb-4 rounded-md"
-      />
-      <h2 className="text-2xl font-semibold mb-2">{title}</h2>
-      <p className="text-gray-300">{description}</p>
-    </div>
+    <a href={link}>
+        <div className="bg-gray-900 text-white p-4 rounded-lg shadow-md hover:scale-105 transform transition-transform duration-300">
+        <img
+            src={image}
+            alt={title}
+            className="w-full h-32 object-cover mb-4 rounded-md"
+        />
+        <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+        <p className="text-gray-300">{description}</p>
+        </div>
+    </a>
   );
 }
 
@@ -20,11 +22,13 @@ function Projects() {
       title: "Ares",
       description: "A simple, lightweight text editor written in one file of pure C.",
       image: "DALL·E 2023-10-04 17.59.47 - a 19th century oil painting of the greek god ares.webp",
+      link: "https://github.com/torbenconto/ares"
     },
     {
       title: "Zeus",
       description: "A powerful, lexer oriented text manipulation tool made in Go",
       image: "zeus.webp",
+      link: "https://github.com/torbenconto/zeus"
     },
   ];
 
