68212ba556fb59206afac59e37f9a6638ba4bced
torbenconto <torben@2x1.dev>
2023-10-08 02:11:55 -0400

    input box

--- src/components/TerminalInput.tsx
+++ src/components/TerminalInput.tsx
@@ -2,10 +2,17 @@ import "./TerminalInput.css"
 
 function TerminalInput() {
     return (
-        <p className="font-semibold flex name-color">torbenconto
-            <p className="font-normal at-symbol-color">@</p>
-            <p className="font-normal location-color">nhhs</p>
-        </p>
+        <>
+        <div className="flex space-x-1">
+            <p className="font-semibold flex name-color">torbenconto
+                <p className="font-normal at-symbol-color">@</p>
+                <p className="font-normal location-color">nhhs</p>
+                <p className="font-semibold arrow-color ml-1">&gt;</p>
+            </p>
+            <input className="input-box px-1">
+            </input>
+        </div>
+        </>
     )
 }
 
